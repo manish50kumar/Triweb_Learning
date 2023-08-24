af0b19c82f907551e42866f993a373c98117e6fb
manish50kumar <manish5060773@gmail.com>
2023-08-24 18:47:59 +0530

    Global method isFinite , isNaN

---
+++ ES6/global.js
@@ -0,0 +1,12 @@
+
+
+console.log(isFinite(2)); // true
+
+console.log(isFinite(2 / 0)); //false
+
+
+console.log("Is NaN: ", isNaN(2)); //false 
+
+console.log("Is NaN: ", isNaN(2/'e')); //true 
+
+
