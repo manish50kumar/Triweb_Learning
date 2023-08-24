ee87396cadac4e296015944e145edab41382ccb7
manish50kumar <manish5060773@gmail.com>
2023-08-24 11:14:17 +0530

    property shorthand

---
+++ ES6/property_shorthand.js
@@ -0,0 +1,20 @@
+// let name = "manish";
+// let age = 22;
+
+// let student = {
+//     name: name,
+//     age:age
+// }
+
+
+// console.log(student);
+let name = "manish";
+let age = 22;
+
+let student = {
+    name,
+    age
+}
+
+
+console.log(student);
