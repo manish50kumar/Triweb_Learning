9dbd44793cbe22e621d8542bb8196cf99b8ef2ef
manish50kumar <manish5060773@gmail.com>
2023-08-24 17:01:35 +0530

    fail soft destructuring

---
+++ ES6/parameter_context_matching.js
@@ -0,0 +1,27 @@
+// Paameter context matching
+
+// function dataType1([sname, age]) {
+//     console.log(sname);
+//     console.log(age);
+// }
+
+// dataType1(["manish", 20]);
+
+
+// function dataType1({ sname, age }) {
+//     console.log(sname);
+//     console.log(age);
+// }
+
+// dataType1({sname:"manish",age: 20});
+
+
+function dataType1({name: ename,age : eage }) {
+    console.log(ename);
+    console.log(eage);
+}
+
+dataType1({name:"manish",age: 20});
+
+
+
