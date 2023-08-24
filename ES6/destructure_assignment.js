a67faad33bc5e717a6b926dfeb24c4b604594e45
manish50kumar <manish5060773@gmail.com>
2023-08-24 16:53:24 +0530

    Deep Matching , Object Matchng

--- ES6/destructure_assignment.js
+++ ES6/destructure_assignment.js
@@ -14,18 +14,81 @@
 
 // Short hand matching , Array matching
 
-function getObj() {
-    let student = {
-        sname: "manish",
-        age:22,
-        level: "B.tech"
-    }
-    return student;
-}
+// function getObj() {
+//     let student = {
+//         sname: "manish",
+//         age:22,
+//         level: "B.tech"
+//     }
+//     return student;
+// }
 
 
 // console.log(  getObj());
 
-var { sname, age, level } = getObj();
+// var { sname, age, level } = getObj();
 
-console.log(sname, age, level);
+// console.log(sname, age, level);
+//---------------------------------------------------------
+// Object matching , Deep matching
+
+
+// function getData() {
+//     let obj = {
+//         ename: "manish",
+//         salary:1000000
+//     }
+//     return obj;
+// }
+
+
+// let { ename, salary } = getData();
+// console.log(ename, salary);
+
+
+// function getData() {
+//     let obj = {
+//         ename: "manish",
+//         salary:1000000
+//     }
+//     return obj;
+// }
+
+
+// let { ename : employeeName, salary:employeeSalary } = getData();
+// console.log(employeeName, employeeSalary);
+
+// Deep matching
+
+// function getData() {
+//     let obj = {
+//         ename: "manish",
+//         salary: 1000000,
+//         location: {
+//             city:"Patna"
+//         }
+//     }
+//     return obj;
+// }
+
+
+// let { ename : employeeName, salary:employeeSalary,location:employeeLocation } = getData();
+// console.log(employeeName, employeeSalary,employeeLocation);
+
+
+function getData() {
+    let obj = {
+        ename: "manish",
+        salary: 1000000,
+        location: {
+            city:"Patna"
+        }
+    }
+    return obj;
+}
+
+
+let { ename : employeeName, salary:employeeSalary,location:{city:employeeCity} } = getData();
+console.log(employeeName, employeeSalary,employeeCity);
+
+
