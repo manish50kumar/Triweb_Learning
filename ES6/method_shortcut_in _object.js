a522d380946dcc2613d5649a3c7ef097bd169da6
manish50kumar <manish5060773@gmail.com>
2023-08-24 11:21:45 +0530

    method shortcut in object

---
+++ ES6/method_shortcut_in _object.js
@@ -0,0 +1,22 @@
+
+
+
+
+// let obj = {
+//     demo: function () {
+//         return "manish";
+//     }
+// }
+
+// console.log(obj.demo());
+
+
+
+let obj = {
+    demo () {
+        return "manish";
+    }
+} 
+
+console.log(obj.demo());
+
