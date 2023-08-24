c8f220eec14125a3a944f38ab4384c69e3b22c5f
manish50kumar <manish5060773@gmail.com>
2023-08-24 10:55:12 +0530

    template literals

---
+++ ES6/template_literals.js
@@ -0,0 +1,26 @@
+// Temlate Literals
+
+// function printdata(sname, age) {
+//     let toPrint = "Student Name is " + sname + " and age is " + age;
+//     console.log(toPrint);
+// }
+
+// let sname = " manish";
+// let age = 22;
+
+// printdata(sname, age);
+
+
+function printdata(sname, age) {
+    let toPrint = `Student Name is  ${sname}   and age is   ${age} `;
+    console.log(toPrint);
+}
+
+let sname = " manish";
+let age = 22;
+
+printdata(sname, age);
+
+
+
+
