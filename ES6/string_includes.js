c3f130e4195ac43917d67b40e99ea8e5edcddfde
manish50kumar <manish5060773@gmail.com>
2023-08-24 18:09:27 +0530

    String methods as include , startwith , endswith

---
+++ ES6/string_includes.js
@@ -0,0 +1,22 @@
+
+// .include()
+
+let str = "This is a good place";
+
+console.log(str.includes("place"));
+console.log(str.includes("Place"));
+console.log(str.includes("lace"));
+console.log(str.includes("laaace"));
+
+
+// start with
+
+console.log(str.startsWith("This"));
+console.log(str.startsWith("That"));
+
+
+//enssWith
+
+console.log(str.endsWith("This"));
+console.log(str.endsWith("place"));
+console.log(str.endsWith("Place"));
