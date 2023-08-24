85b5cac8bd38dbe8e54e5c65ed887129e21d5a5d
manish50kumar <manish5060773@gmail.com>
2023-08-24 18:34:49 +0530

    math function

---
+++ ES6/mathes.js
@@ -0,0 +1,25 @@
+// Math.trunc()
+
+
+// console.log(Math.trunc(5));
+// console.log(Math.trunc(5.7));
+// console.log(Math.trunc(5.1));
+// console.log(Math.trunc(-5.9));
+
+
+// // Math.sign()
+
+// console.log(Math.sign(-19));
+// console.log(Math.sign(-20.45));
+// console.log(Math.sign(19));
+// console.log(Math.sign(0));
+
+// // Math.cbrt()
+
+// console.log(Math.cbrt(64));
+
+// Math.log2()  , Math.log10()
+
+console.log(Math.log2(2));
+console.log(Math.log10(2));
+console.log(Math.log10(10));
