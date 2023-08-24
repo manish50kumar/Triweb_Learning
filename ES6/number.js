e699f777e3f4b5ff845ee583bad9f303fa946187
manish50kumar <manish5060773@gmail.com>
2023-08-24 18:43:21 +0530

    Number Methods

---
+++ ES6/number.js
@@ -0,0 +1,29 @@
+// // EPSILON
+
+// let x = Number.EPSILON;
+// console.log(x);
+
+// // MAX Safe INTEGER
+
+// let y = Number.MAX_SAFE_INTEGER;
+// console.log(y);
+
+// // MIN Safe INTEGER
+
+// let z = Number.MIN_SAFE_INTEGER;
+// console.log(z);
+
+
+// Number Method
+
+let a = 123;
+let b = 123.45;
+
+// console.log(Number.isInteger(a));
+// console.log(Number.isInteger(b));
+
+
+let c = 9007199254740991;
+let d = 9007199254740992;
+console.log(Number.isInteger(c));
+console.log(Number.isSafeInteger(d));
