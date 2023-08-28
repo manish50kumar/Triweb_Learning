3bb0273a6e5159392ced84febdb66053276d9455
manish50kumar <manish5060773@gmail.com>
2023-08-28 10:40:41 +0530

    if elif else

---
+++ NodeJS II/python/ifelse.py
@@ -0,0 +1,24 @@
+
+# if 
+# age = 20
+# if age > 18:
+#     print("You can vote")
+
+# if elif
+
+# age1= 23
+# if age1 > 18:
+#     print("Ypu can vote..")
+# else :
+#     print("You can not vote")
+
+a=20
+b=30
+c=40
+
+if a>b and a>c:
+    print("a is greater")
+elif b>c:
+    print("b is greater")
+else:
+    print("c is greater")        
