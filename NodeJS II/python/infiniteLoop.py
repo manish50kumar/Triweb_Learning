86843ec0f2f0f43bb2e44336c1cefc75196789db
manish50kumar <manish5060773@gmail.com>
2023-08-28 11:54:48 +0530

    infinite loop

---
+++ NodeJS II/python/infiniteLoop.py
@@ -0,0 +1,16 @@
+
+
+while 1:
+    print("Enter 1 for A")
+    print("Enter 2 for B")
+    print("Enter 3 for C")
+    print("Enter 0 for exit")
+    inputNum = int(input("Enter a number: " ))
+    if inputNum==1:
+        print("A")
+    if inputNum==2:
+        print("B")
+    if inputNum==3:
+        print("C")
+    if inputNum == 0:
+        break    
