9659836970bff5cfc055569e2162ed9734664df8
manish50kumar <manish5060773@gmail.com>
2023-08-28 10:49:11 +0530

    loop

---
+++ NodeJS II/python/loop.py
@@ -0,0 +1,34 @@
+# for
+# i=0
+# n=10
+# for i in range (n):
+#     print(i+1)
+
+#   
+# check even or odd
+# i=1
+# n=10
+# for i in range (n):
+#     if i % 2 == 0:
+#         print(i , " is even")
+#     else:
+#         print(i , " is odd");  
+# 
+# check even upto n input
+
+# i=1
+# n = int(input("Enter a number : ")) 
+# for i in range(n):
+#     if i % 2==0:
+#         print(i," is even")
+
+
+# check odd upto n
+
+i=1
+n = int(input("Enter a number : ")) 
+for i in range(n):
+    if i % 2!=0:
+        print(i," is odd")
+
+   
