f2fa7cde4372dc2ea26e95002f4d394fe9cbc632
manish50kumar <manish5060773@gmail.com>
2023-09-07 12:40:26 +0530

    Menu use Link

--- ReactJs/demo/src/App.js
+++ ReactJs/demo/src/App.js
@@ -5,12 +5,15 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
+import MainMenu from './components/layouts/MainMenu'
+
 import {Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div >
+      <MainMenu></MainMenu>
       <Routes>
           <Route path='/' element={<Home/>}></Route> 
           <Route path='/about' element={<About/>}></Route> 
