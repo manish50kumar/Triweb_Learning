6569d93b2678ade503b92f26a16cfc77c01dba74
manish50kumar <manish5060773@gmail.com>
2023-09-08 20:08:23 +0530

    React Hook useRef

--- ReactJs/basic_react_project/src/components/product/AddProductForm.js
+++ ReactJs/basic_react_project/src/components/product/AddProductForm.js
@@ -1,28 +1,51 @@
 import Styles from './AddProductForm.module.css'
 import CustomWrapper from '../layouts/CustomWrapper';
 
+import { useRef } from 'react';
+
 function AddProductForm() {
+    const product_name_ref = useRef();
+    const image_ref = useRef();
+    const price_ref = useRef();
+    const description_ref = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const product_name = product_name_ref.current.value;
+        const image = image_ref.current.value;
+        const price = price_ref.current.value;
+        const description = description_ref.current.value;
+
+        const product = {
+            product_name,
+            image,
+            price,
+            description
+        }
+        console.log(product);
+    }
+
     return (
         <CustomWrapper>
-            <form className={Styles.form}>
+            <form className={Styles.form} onSubmit={submitHandler}>
                 <div className={Styles.groups}>
                     <label htmlFor='product_name'> Product Name</label>
-                    <input type='text' placeholder='' id='product_name'></input>
+                    <input type='text' placeholder='' id='product_name' ref={product_name_ref}></input>
                    
                 </div>
                 <div className={Styles.groups}>
                     <label htmlFor='image'>Image</label>
-                     <input type='text' placeholder='' id='image'></input>
+                     <input type='text' placeholder='' id='image' ref={image_ref}></input>
                     
                 </div>
                 <div className={Styles.groups}>
                     <label htmlFor='price'>Price</label>
-                    <input type='text' placeholder='' id='price'></input>
+                    <input type='text' placeholder='' id='price' ref={price_ref}></input>
                     
                 </div>
                 <div className={Styles.groups}>
                     <label htmlFor='description'>Description</label>
-                    <textarea rows='5' id='description'></textarea>
+                    <textarea rows='5' id='description' ref={description_ref}></textarea>
                 </div>
                 <div className={Styles.btn}>
                     <button>ADD</button>
