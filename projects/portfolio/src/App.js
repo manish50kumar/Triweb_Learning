be4c8113d08dbe25d9de1813dc0553ff5e1e2bd5
manish50kumar <manish5060773@gmail.com>
2023-09-24 12:28:43 +0530

    Add project section as portfolio

--- projects/portfolio/src/App.js
+++ projects/portfolio/src/App.js
@@ -2,6 +2,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar"
+import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <div>
       <NavBar />
       <Home />
-      <About/>
+      <About />
+      <Portfolio />
+      
       <SocialLinks/>
     </div>
   );
