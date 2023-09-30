540f133326fb742d7067b58d5f5217c89a025532
manish50kumar <manish5060773@gmail.com>
2023-09-30 12:19:43 +0530

    work on portfolio button in home page and remove warning from socialmedialink

--- projects/portfolio/src/components/SocialLinks.jsx
+++ projects/portfolio/src/components/SocialLinks.jsx
@@ -55,7 +55,7 @@ const SocialLinks = () => {
           <ul>
               
               {links.map(({id,child,href,style,download}) => {
-               return  <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " "+style}>
+               return  <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500  ${style} `}>
                     <a href={href} className="flex justify-between items-center w-full text-white"
                         download={download}
                         target="_blank"
